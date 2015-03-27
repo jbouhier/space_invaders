98e0935bab886aa13cf14d7b599c820af62820a0
jbouhier <jb.bouhier@gmail.com>
2015-03-27 21:12:34 +0100

    First commit, basic SDL window

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project Space Invaders
+etna-alternance.net - Software Engineerers - March 2017 promotion
+
+## Presentation
+	Remake of three classic retro games with lib SDL 2.0
+	1- Space Invaders (1978)
+	2- Galaga (1981)
+	3- R-Type (1987) 	  
+	Last C project of ETNA 1st year after the remaking of ls, cat and printf 
+
+
+## Team
+**Jean-Baptiste Bouhier:** Developer & Project Manager
+
+**Gatien Mouafo:** Developer
+
+**Thimothee Lestin:** Developer
+
+**Manel Zarali:** Developer
+
--- space_invaders/main.c
+++ space_invaders/main.c
@@ -7,9 +7,62 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, const char * argv[])
+{
+    
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init( SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS ) < 0)
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        exit(-1);
+    }
+
+    //Create window
+    window = SDL_CreateWindow( "SDL Tutorial",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SCREEN_WIDTH,
+                              SCREEN_HEIGHT,
+                              SDL_WINDOW_SHOWN );
+    if( window == NULL )
+    {
+        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Get window surface
+        screenSurface = SDL_GetWindowSurface( window );
+
+        //Fill the surface with a color
+        SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x33, 0x66) );
+
+        //Update the surface
+        SDL_UpdateWindowSurface( window );
+
+        //Wait two seconds
+        SDL_Delay( 2000 );
+    }
+
+
+    //Destroy window
+    SDL_DestroyWindow( window );
+    
+    //Quit SDL subsystems
+    SDL_Quit();
+    
     return 0;
 }
