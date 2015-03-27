6c43591f3e9011aee64c99c4877cf46fe482e8f2
jbouhier <jb.bouhier@gmail.com>
2015-03-27 20:11:37 +0100

    Initial Commit

---
+++ space_invaders/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  space_invaders
+//
+//  Created by SynxS on 27/03/15.
+//  Copyright (c) 2015 ETNA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
