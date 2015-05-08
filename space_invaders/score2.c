ad17c23b363c7705501ce9ac4bd0dbe24ffa76ba
jbouhier <jb.bouhier@gmail.com>
2015-05-08 13:36:41 +0200

    Feature score: score_str finished

--- space_invaders/score2.c
+++ space_invaders/score2.c
@@ -6,4 +6,21 @@
 //  Copyright (c) 2015 ETNA. All rights reserved.
 //
 
-#include "score2.h"
+#include "main_file.h"
+
+char  *score_str(long hscore, char *score_str)
+{
+    size_t  len;
+    size_t  i;
+    char    *tmp;
+    
+    sprintf(score_str, "%ld", hscore);
+    len = strlen(score_str);
+    tmp = malloc(sizeof(char) * 4 + 1);
+    
+    for (i = 0; i < SCORE_LENGTH - len; i++)
+        strcat(tmp, "0");
+    
+    strcat(tmp, score_str);
+    return (tmp);
+}
