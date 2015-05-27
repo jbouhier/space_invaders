3afef576619a6678d642565fb9db5ba90481c362
jbouhier <jb.bouhier@gmail.com>
2015-05-27 14:15:22 +0200

    Fix main: improved norme

--- space_invaders/main.c
+++ space_invaders/main.c
@@ -24,9 +24,9 @@ int     main(void)
     game = init_screen(game);
     game = showBegin(game, &(game.text));
     
-    while(game.quit != 1)
+    while (game.quit != 1)
     {
-        while( SDL_PollEvent(&game.Gevenements) >= 0 && game.quit != 1 )
+        while (SDL_PollEvent(&game.Gevenements) >= 0 && game.quit != 1)
         {
             tempsActuel = SDL_GetTicks();
             game = handleEvent(game);
@@ -39,20 +39,20 @@ int     main(void)
             }
             else
             {
-                if (game.begin.state == 0 && gameover == 0 && game.quit != 2) {
+                if (game.begin.state == 0 && gameover == 0 && game.quit != 2)
                     game = showGame(game, tempsActuel, tempsPrecedent);
-                }
-                tempsPrecedent = tempsActuel; /* Le temps "actuel" devient le temps "precedent" pour nos futurs calculs */
+                // Le temps "actuel" devient le temps "precedent" pour nos futurs calculs
+                tempsPrecedent = tempsActuel;
             }
             if (game.Gplayer1.lives == -1 || game.Gplayer2.lives == -1 || game.Gmonster[0].monster == NULL)
                 gameover = 1;
             
             toWait = SDL_GetTicks() - tempsActuel;
-            if ( toWait < 16 && game.quit != 1)
+            if (toWait < 16 && game.quit != 1)
                 SDL_Delay(16 - toWait);
         }
     }
 
     end(game);
-    return 0;
+    return (0);
 }
